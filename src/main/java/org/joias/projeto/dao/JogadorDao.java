dfd6333a51452d43cbf72d89382aa4f57f05221f
rootdanley <danleyalves@hotmail.com>
2024-09-28 13:45:55 -0300

    feat: jogadorDAO

--- src/main/java/org/joias/projeto/dao/JogadorDao.java
+++ src/main/java/org/joias/projeto/dao/JogadorDao.java
@@ -1,4 +1,19 @@
 package org.joias.projeto.dao;
 
+import org.joias.projeto.models.Jogador;
+import org.joias.projeto.models.Ranking;
+
+import java.util.List;
+
 public class JogadorDao {
+
+
+    public void salvarVencedor(Jogador jogador) {
+        // TODO
+    }
+
+    public List<Ranking> buscarRanking() {
+        // TODO
+        return null;
+    }
 }
